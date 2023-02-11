b82d12b9a4ca282c40c6194792de36ab773b20c0
Manuel Riezebosch <mriezebosch@gmail.com>
2023-02-11 08:16:19 +0100

    refactor(example): separate project

---
+++ Examples/Iffy.Examples.Rebus/Program.cs
@@ -0,0 +1,28 @@
+using Azure.Identity;
+using Iffy;
+using Rebus.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services
+    .AddRebus(configure => configure
+        .Transport(t => t.UseAzureServiceBus("", "", new DefaultAzureCredential())
+            .AutomaticallyRenewPeekLock()
+            .SetDuplicateDetectionHistoryTimeWindow(new TimeSpan(0, 2, 0))
+            .If(!builder.Environment.IsDevelopment(), settings => settings
+                .DoNotCreateQueues()
+                .DoNotCheckQueueConfiguration())
+            .If(builder.Environment.IsDevelopment(),
+                then => then
+                    .SetAutoDeleteOnIdle(TimeSpan.FromMinutes(5)),
+                @else => @else
+                    .DoNotCreateQueues()
+                    .DoNotCheckQueueConfiguration())
+            .If(builder.Environment.IsDevelopment())
+            .Then(settings => settings
+                .SetAutoDeleteOnIdle(TimeSpan.FromMinutes(5)))
+            .Else(settings => settings
+                .DoNotCreateQueues()
+                .DoNotCheckQueueConfiguration())));
+
+var app = builder.Build();
+app.Run();
